cd37a2802f9c2da91e7d1bd3b7e847b02f285fbb
shanmugarajsekar <shanmugaraj2003@gmail.com>
2023-12-31 19:24:38 +0530

    updated svg like

--- app/static-component/Svg.jsx
+++ app/static-component/Svg.jsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import React from "react";
 import svg from "../images/wave1.svg";
 export default function Svg() {
   return (
-    <Image
+    <img
       src={svg}
       alt="wave"
       height={0}
       width={0}
       className="w-full object-cover"
-    ></Image>
+    ></img>
   );
 }
