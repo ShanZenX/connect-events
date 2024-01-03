cc431571c7daf02104e89ce4761ea865fa633a6d
shanmugarajsekar <shanmugaraj2003@gmail.com>
2024-01-03 21:24:53 +0530

    updated client images

--- app/components/HomeComponents/Client.jsx
+++ app/components/HomeComponents/Client.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ClientModule from "../modules/ClientModule";
 import Heading from "../Heading";
-import Image from "next/image";
 import client1 from "@/app/images/client-img/client-1.jpeg";
-import client2 from "@/app/images/client-img/client-2.jpeg";
+import client2 from "@/app/images/client-img/client-2.jpg";
 import client3 from "@/app/images/client-img/client-3.jpeg";
-import client4 from "@/app/images/client-img/client-4.jpeg";
+import client4 from "@/app/images/client-img/client-4.jpg";
 import client5 from "@/app/images/client-img/client-5.jpeg";
 import client6 from "@/app/images/client-img/client-6.jpeg";
-import client7 from "@/app/images/client-img/client-7.jpeg";
+import client7 from "@/app/images/client-img/client-7.jpg";
 import client8 from "@/app/images/client-img/client-8.jpeg";
 import client9 from "@/app/images/client-img/client-9.jpeg";
+import client10 from "@/app/images/client-img/client-10.jpg";
+import client11 from "@/app/images/client-img/client-11.jpg";
 
 export default function Client() {
   return (
@@ -26,6 +27,9 @@ export default function Client() {
         <ClientModule image={client7} />
         <ClientModule image={client8} />
         <ClientModule image={client9} />
+        <ClientModule image={client10} />
+        <ClientModule image={client11} />
+
       </div>
     </div>
   );
--- app/components/modules/ClientModule.jsx
+++ app/components/modules/ClientModule.jsx
@@ -5,10 +5,7 @@ export default function ClientModule(props) {
   return (
     <Image
       src={props.image}
-      width={100}
-      height={100}
       className=" w-[120px] sm:w-[250px] sm:h-[100px] m-7"
-      quality={100}
       alt="client-img"
     ></Image>
   );
